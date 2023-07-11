fbcdec097456ac363b6f37064f623d474803f6e1
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2023-07-11 12:27:42 -0600

    Update index values

--- Data Structures/Arrays/tic_tac_toe.py
+++ Data Structures/Arrays/tic_tac_toe.py
@@ -52,13 +52,13 @@ while True:
             p1_row, p1_col = input('Player 1 - Select row and column of your move (separated by a comma):').split(",", 1)
             p1_row = int(p1_row)
             p1_col = int(p1_col)
-            if not (1 <= p1_row <= 3) or not (1 <= p1_col <= 3) or board[p1_row - 1][p1_col - 1] != '.':
+            if not (0 <= p1_row <= 2) or not (0 <= p1_col <= 2) or board[p1_row][p1_col] != '.':
                 raise ValueError
         except ValueError:
             print("Invalid move. Please try again.")
             continue
 
-        board[p1_row - 1][p1_col - 1] = "X"
+        board[p1_row][p1_col] = "X"
         print_board(board)
         if check_win(board):
             win = True
@@ -74,13 +74,13 @@ while True:
                 p2_row, p2_col = input('Player 2 - Select row and column of your move (separated by a comma):').split(",", 1)
                 p2_row = int(p2_row)
                 p2_col = int(p2_col)
-                if not (1 <= p2_row <= 3) or not (1 <= p2_col <= 3) or board[p2_row - 1][p2_col - 1] != '.':
+                if not (0 <= p2_row <= 2) or not (0 <= p2_col <= 2) or board[p2_row][p2_col] != '.':
                     raise ValueError
             except ValueError:
                 print("Invalid move. Please try again.")
                 continue
 
-            board[p2_row - 1][p2_col - 1] = "O"
+            board[p2_row][p2_col] = "O"
             print_board(board)
             if check_win(board):
                 win = True
